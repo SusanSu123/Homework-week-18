6905bab1d05dd713b636e82ab766a14e5540513c
Susan Su <suchangau@hotmail.com>
2021-07-04 21:20:27 +1000

    make it better

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercise", {
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
-
 });
 
 // routes
