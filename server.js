8aae226594a0b95d7eb93483fc2956e8e149cd0b
Susan Su <suchangau@hotmail.com>
2021-07-04 14:48:37 +1000

    deploying

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
-    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 
 });
 
